b331e421360680e22e7bd193ca4032c2abbda2da
danidin12 <82769818+danidin12@users.noreply.github.com>
2021-05-24 13:42:46 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 if __name__ == "__main__":
     #set site to check
-    site="http://www.google.com"
+    site = "http://www.google.com"
     resp = req.get(site)
     if resp.status_code != 200:
         msg = f" {site} with error {resp.status_code} "
